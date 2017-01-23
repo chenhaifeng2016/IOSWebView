ed336c00e751268427299afb6da1e345f12e838a
chenhf <chenhf2010@qq.com>
2017-01-23 18:14:54 +0800

    Initial Commit

---
+++ IOSWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSWebView
+//
+//  Created by chenhf on 2017/1/23.
+//  Copyright © 2017年 cssweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSWebView
+//
+//  Created by chenhf on 2017/1/23.
+//  Copyright © 2017年 cssweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
